41e52e73f83555a84a75a9f8c759ce9464a3533f
felipefig21 <phillfigueiredo@gmail.com>
2025-12-12 00:11:47 -0300

    Fix: Remove grav150.png import and usage from Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,8 @@
 import './Hero.css'
-import heroimg from './grav150.png'
 
 function Hero() {
   return (
     <section className="hero">
-      <img
-        src={heroimg}
-        alt="Arte em destaque"
-        className="hero-image"
-        onError={(e) => {
-          console.log('Erro ao carregar imagem')
-          e.target.style.backgroundColor = '#333'
-        }}
-      />
     </section>
   )
 }
