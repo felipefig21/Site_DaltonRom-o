1500fa087fe56a4356d9fc573f67b0ceb4cea50a
felipefig21 <phillfigueiredo@gmail.com>
2025-12-13 17:29:38 -0300

    akeitei tudo

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,9 @@
 import './Hero.css'
-
+import imagem from './grav150.png'
 function Hero() {
   return (
     <section className="hero">
+      <img src={imagem} alt="" className='hero-image'/>
     </section>
   )
 }
